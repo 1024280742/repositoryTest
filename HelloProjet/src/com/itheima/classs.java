b65f80eb359fb08e2648256779f09c0174892020
frank <1024280742@qq.com>
2018-11-24 20:05:30 +0800

    第一次提交本地工程

---
+++ HelloProjet/src/com/itheima/classs.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class classs {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
